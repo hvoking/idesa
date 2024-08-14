6d2039a26263b9bc97fc30f2bdbf8e58bbfdc597
hvoking <gus@ugeom.com>
2024-08-14 13:59:13 -0400

    backup 14-08-24

--- src/components/main/maps/popup/index.tsx
+++ src/components/main/maps/popup/index.tsx
@@ -18,25 +18,45 @@ export const CustomPopup: any = ({ marker, setPropertyInfo }: any) => {
         latitude={latitude}
         closeOnClick={false}
         onClose={onClose}
-        closeOnMove={true}
         offset={20}
       >
           <div className="popup-wrapper">
-            <div style={{paddingTop: "20px", borderBottom: "2px solid rgba(126, 126, 132, 0.2)"}}></div>
-            <div>{marker.lugar}</div>
-            <div style={{fontSize: "2.4em", fontWeight: "600"}}>{marker.nombre}</div>
-            <div style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}>
-              <div style={{display: "grid"}}>
-                <div>LOTES</div>
-                <div>{marker.lotes}</div>
-              </div>
-              <div style={{display: "grid"}}>
-                <div>DISPONIBLES</div>
-                <div>{marker.disponibles}</div>
+            <div style={{paddingTop: "20px", borderBottom: "2px solid rgba(126, 126, 132, 0.2)"}}>
+              <div className="search-cross" style={{position: "absolute", right: "10px", top: "10px"}}>
+                  <svg viewBox="0 0 15 15" onClick={onClose}>
+                      <line
+                          x1={0}
+                          y1={0}
+                          x2={15}
+                          y2={15}
+                          stroke="rgba(0, 0, 0, 0.6)"
+                          strokeWidth="2"
+                      />
+                      <line
+                          x1={15}
+                          y1={0}
+                          x2={0}
+                          y2={15}
+                          stroke="rgba(0, 0, 0, 0.6)"
+                          strokeWidth="2"
+                      />
+                  </svg>
               </div>
             </div>
-            <div>DESDE {marker.desde}</div>
-            <div>Ir a la Fracción</div>
+            <div style={{paddingTop: "10px", color: "rgba(126, 126, 132, 1)"}}>{marker.lugar.toUpperCase()}</div>
+            <div style={{fontSize: "3.2em", fontWeight: "500"}}>{marker.nombre.toUpperCase()}</div>
+            <div style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}>
+              <div style={{display: "grid"}}>
+                <div style={{color: "rgba(126, 126, 132, 1)"}}>LOTES</div>
+                <div><strong>{marker.lotes}</strong></div>
+              </div>
+              <div style={{display: "grid"}}>
+                <div style={{color: "rgba(126, 126, 132, 1)"}}>DISPONIBLES</div>
+                <div><strong>{marker.disponibles}</strong></div>
+              </div>
+            </div>
+            <div>DESDE <span style={{color: "rgba(255, 130, 48, 1)", fontWeight: "600", fontSize: "1.1em"}}>{marker.desde}</span></div>
+            <div className="search-cta">Ir a la Fracción</div>
           </div>
       </Popup>
     )
